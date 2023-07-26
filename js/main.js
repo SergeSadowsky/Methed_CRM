69de098a7b7f78f7d99e49f109c9994a887c6b94
uncleserge <serge@sadowsky.spb.ru>
2023-07-26 13:11:25 +0300

    m2t2 finished

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modal = document.querySelector('div.overlay__modal.modal');
+const modalHeader = modal.querySelector('h2');
+const modalForm = modal.querySelector('form');
+const modalCBox = modal.querySelector('#discount');
+const modalDiscInput = modal.querySelector('input[name="discount_count"]');
