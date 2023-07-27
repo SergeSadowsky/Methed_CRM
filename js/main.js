3542cb6f15e85cf86db30547ad780ac10998eeaa
uncleserge <serge@sadowsky.spb.ru>
2023-07-27 20:47:09 +0300

    m2t2 selectors fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const modal = document.querySelector('div.overlay__modal.modal');
-const modalHeader = modal.querySelector('h2');
-const modalForm = modal.querySelector('form');
-const modalCBox = modal.querySelector('#discount');
-const modalDiscInput = modal.querySelector('input[name="discount_count"]');
+// const modal = document.querySelector('div.overlay__modal.modal');
+// const modalHeader = modal.querySelector('h2');
+// const modalForm = modal.querySelector('form');
+// const modalCBox = modal.querySelector('#discount');
+// const modalDiscInput = modal.querySelector('input[name="discount_count"]');
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckBox = document.querySelector('.modal__checkbox');
+const modalCheckBoxText = document.querySelector('.modal__input_discount');
